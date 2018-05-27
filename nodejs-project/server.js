1824dd4d9174185e66b4ad82508a5461dfa96236
alexcpu7 <alexcpu7@gmail.com>
2018-05-27 21:16:11 +0300

    dev

--- nodejs-project/server.js
+++ nodejs-project/server.js
@@ -7,9 +7,8 @@ var db;
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
-/*app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));*/
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* == pages == */
 app.get('/', function(req, res){
@@ -28,14 +27,8 @@ app.get('/chat', function (req, res) {
     res.render('chat');
 });
 
-app.get('/form', function (req, res) {
-    res.render('form');
-});
-
-app.post('/form', function(req, res){
-    if (!req.body) return res.sendStatus(400);
-    console.log(req.body);
-    res.redirect('form');
+app.get('/react', function (req, res) {
+    res.render('react');
 });
 
 app.get('/new/:id', function(req, res){
@@ -46,14 +39,41 @@ app.get('/new/:id', function(req, res){
     });
 });
 
-app.use( function(req, res){
+app.get('/forms', function (req, res) {
+    /*db.collection('form').find().toArray(function (err, docs) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(docs);
+    });*/
+    res.render('forms');
+});
+
+app.post('/forms', function(req, res){
+    if (!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    var form = {
+        'email' : req.body.email,
+        'descr' : req.body.descr
+    };
+    db.collection('form').insert(form, (req, res) => {
+        if (err){
+            console.log(err);
+            res.sendStatus(500);
+        }
+        res.send(form);
+    });
+
+    res.redirect('forms');
+});
+
+/*app.use( function(req, res){
     req.status(500);
     res.send(500);
-});
-app.listen(1488, '127.0.0.1', function(){
-    console.log('Сервер: 127.0.0.1:1488 запущен...');
-});
-/*mongoClient.connect('mongodb://127.0.0.1:27017', function (err, database) {
+});*/
+
+mongoClient.connect('mongodb://127.0.0.1:27017', function (err, database) {
     if (err){
         console.log('Произошла ошибка подключения к MongoDB:');
         return console.log(err);
@@ -61,6 +81,9 @@ app.listen(1488, '127.0.0.1', function(){
     db = database;
     if (db){
         console.log('Соединение с MongoDB успешно установленно');
+        app.listen(1488, '127.0.0.1', function(){
+            console.log('Сервер: 127.0.0.1:1488 запущен...');
+        });
 
     }
-});*/
+});

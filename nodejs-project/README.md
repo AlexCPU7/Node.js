fbe3c139e13b7a984463b647c70680b580cfe3d0
Alexcpu7 <alexcpu7@gmail.com>
2018-06-03 16:19:35 +0300

    dev

--- nodejs-project/README.md
+++ nodejs-project/README.md
@@ -1,6 +1,10 @@
 Проект написанный на Node.js
 
 Установка:
+npm install
+
+MongoDB:
+mongod --dbpath ...\...\Node.js\nodejs-project\dumpdb
 
 Запуск:
 node server.js
--- nodejs-project/server.js
+++ nodejs-project/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+var logger = require('morgan');
+var silent = process.env.NODE_ENV === 'test';
 
 const app = express();
 var db;
@@ -10,28 +12,34 @@ app.use('/public', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.enable('verbose errors');
+
+if (app.settings.env === 'production') app.disable('verbose errors');
+
+silent || app.use(logger('dev'));
+
 /* == pages == */
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.render('index', {date: new Date()});
 });
 
-app.get('/about', function(req, res){
+app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/test', function(req, res){
+app.get('/test', (req, res) => {
   res.render('test');
 });
 
-app.get('/chat', function (req, res) {
+app.get('/chat', (req, res) => {
     res.render('chat');
 });
 
-app.get('/react', function (req, res) {
+app.get('/react', (req, res) => {
     res.render('react');
 });
 
-app.get('/new/:id', function(req, res){
+app.get('/new/:id',(req, res) => {
     console.log(req.query);
     res.render('new', {
         newId: req.params.id,
@@ -39,7 +47,7 @@ app.get('/new/:id', function(req, res){
     });
 });
 
-app.get('/forms', function (req, res) {
+app.get('/forms', (req, res) => {
     /*db.collection('form').find().toArray(function (err, docs) {
         if (err){
             console.log(err);
@@ -50,13 +58,28 @@ app.get('/forms', function (req, res) {
     res.render('forms');
 });
 
-app.post('/forms', function(req, res){
+app.post('/forms', (req, res) => {
     if (!req.body) return res.sendStatus(400);
+
     console.log(req.body);
+
+    var collection = db.collection('form');
+
     var form = {
         'email' : req.body.email,
         'descr' : req.body.descr
     };
+    collection.insertOne(form, (err, res) => {
+        if (err){
+            console.log(err);
+            return;
+        }
+
+        console.log(res.ops);
+        db.close();
+    });
+
+
     db.collection('form').insert(form, (req, res) => {
         if (err){
             console.log(err);
@@ -68,22 +91,56 @@ app.post('/forms', function(req, res){
     res.redirect('forms');
 });
 
-/*app.use( function(req, res){
-    req.status(500);
-    res.send(500);
-});*/
+app.get('/404', function(req, res, next){
+    next();
+});
 
-mongoClient.connect('mongodb://127.0.0.1:27017', function (err, database) {
+app.get('/403', function(req, res, next){
+    // trigger a 403 error
+    var err = new Error('not allowed!');
+    err.status = 403;
+    next(err);
+});
+
+app.get('/500', function(req, res, next){
+    // trigger a generic (500) error
+    next(new Error('keyboard cat!'));
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+
+    res.format({
+        html: function () {
+            res.render('404', { url: req.url })
+        },
+        json: function () {
+            res.json({ error: 'Not found' })
+        },
+        default: function () {
+            res.type('txt').send('Not found')
+        }
+    })
+});
+
+app.use(function(err, req, res, next){
+    // we may use properties of the error object
+    // here and next(err) appropriately, or if
+    // we possibly recovered from the error, simply next().
+    res.status(err.status || 500);
+    res.render('500', { error: err });
+});
+
+MongoClient.connect('mongodb://127.0.0.1:27017/testdb', (err, db) => {
     if (err){
         console.log('Произошла ошибка подключения к MongoDB:');
         return console.log(err);
     }
-    db = database;
     if (db){
         console.log('Соединение с MongoDB успешно установленно');
         app.listen(1488, '127.0.0.1', function(){
             console.log('Сервер: 127.0.0.1:1488 запущен...');
         });
-
+        db.close();
     }
 });
